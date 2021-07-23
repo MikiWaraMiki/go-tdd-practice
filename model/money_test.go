f994fb22f6bf61b885e45a328e1d93529ebe69a4
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-07-23 15:29:37 +0900

    refactor テストコードでDollarの副作用がなくなるように修正

--- model/money_test.go
+++ model/money_test.go
@@ -5,8 +5,13 @@ import "testing"
 func TestMoneyMultiplication(t *testing.T) {
 	five := NewDollar(5)
 
-	five.Times(2)
+	product := five.Times(3)
 
+	if product.Amount() != 15 {
+		t.Errorf("expected: 15, result:%v", product.Amount)
+	}
+
+	product = five.Times(2)
 	if five.Amount() != 10 {
 		t.Errorf("expected: 10, result: %v", five.Amount())
 	}
