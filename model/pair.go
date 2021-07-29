b94de0405c181c88a70302159d6d6fdaae822d47
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-07-29 19:54:19 +0900

    add Pairの等価判定

--- model/pair.go
+++ model/pair.go
@@ -1,5 +1,9 @@
 package model
 
+type IPair interface {
+	GetFrom() string
+	GetTo() string
+}
 type Pair struct {
 	from string
 	to   string
@@ -10,4 +14,28 @@ func NewPair(from string, to string) *Pair {
 		from: from,
 		to:   to,
 	}
+}
+
+func (this Pair) GetFrom() string {
+	return this.from
+}
+
+func (this Pair) GetTo() string {
+	return this.to
+}
+
+func (this Pair) Equals(obj IPair) bool {
+	if obj == nil {
+		return false
+	}
+
+	if this.from != obj.GetFrom() {
+		return false
+	}
+
+	if this.to != obj.GetTo() {
+		return false
+	}
+
+	return true
 }
